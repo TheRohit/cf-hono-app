afb3c5d721611f0994c84792ea09a16584d7647f
TheRohit <rohitmeshram000@gmail.com>
2024-09-02 14:21:33 +0530

    get put kv

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,20 @@ app.get("/", (c) => {
   return c.text("Hello , the deploy works!");
 });
 
-app.put("/add/:key", async (c) => {
+app.put("/put/:key", async (c) => {
   const key = c.req.param("key");
   const body = await c.req.text();
   await c.env.KV.put(key, body);
   return c.text(`Put ${key} successfully!`);
+});
+
+app.get("/get/:key", async (c) => {
+  const key = c.req.param("key");
+  const value = await c.env.KV.get(key);
+  if (value === null) {
+    return c.text(`Key ${key} not found`, 404);
+  }
+  return c.text(value);
 });
 
 export default app;
