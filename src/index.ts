931e8be8093dff7b5dc4bf7acce7803317306ac6
TheRohit <rohitmeshram000@gmail.com>
2024-09-02 13:37:59 +0530

    test ci

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Cloudflare Workers!");
+  return c.text("Hello , the deploy works!");
 });
 
 export default app;
